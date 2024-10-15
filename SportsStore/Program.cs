ee0c2c1b01159efdea214b84a48aa9bad9d05fdb
Janvi <janvimarakna1304@gmail.com>
2024-10-15 18:23:18 +0530

    Add project files.

---
+++ SportsStore/Program.cs
@@ -0,0 +1,37 @@
+namespace SportsStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();//enables support for serving conent from wwwroot folder
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(  
+            // register the MVCFramework as a source of endpoints using a default convention for mapping request to classes and methods
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
